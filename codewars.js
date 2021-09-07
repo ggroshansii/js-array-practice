2635e8f0a5171438e8d60ea76e4d9c0c33e6a364
ggroshansii <garthgroshansii@gmail.com>
2021-09-07 14:05:43 -0400

    codewars challenge #4 completed

--- codewars.js
+++ codewars.js
@@ -31,12 +31,15 @@ function flyBy(lamps, drone) {
         : "o".repeat(drone.length) + "x".repeat(lamps.length - drone.length);
 }
 
-
 // # 4
 // Get the mean of an array
 // https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
 
-
+function getAverage(marks){
+    return Math.floor((marks.reduce((accum, element) => {
+        return accum + element
+    })) / marks.length);
+}
 
 
 // #5
