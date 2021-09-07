651b3b722dd7d2060c712d38c151b69e1fefa3ed
ggroshansii <garthgroshansii@gmail.com>
2021-09-07 13:21:49 -0400

    completed codewars challenge 1

--- codewars.js
+++ codewars.js
@@ -2,7 +2,16 @@
 // Testing 1-2-3
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 
+var number = function(array){
+    let count = 1;
+    let newArr = [];
+    array.forEach((element) => {   
+        newArr.push(`${count}: ${element}`);
+        count++;
+    });
 
+    return newArr;
+  }
 
 
 // # 2
