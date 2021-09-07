c7ddbef149a9c144822739fb4004ccc0bb471962
ggroshansii <garthgroshansii@gmail.com>
2021-09-07 15:58:41 -0400

    function exercise #6 complete

--- main.js
+++ main.js
@@ -75,13 +75,18 @@ function findUniqueVals(arr) {
 
 // Put your answer below -------------------------
 
-
-
-
-
-
-
-
+function arrEquality(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    } else {
+        for (let i=0;i<arr1.length;i++){
+            if (!arr2.includes(arr1[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
 
 // -----------------------------------------------
